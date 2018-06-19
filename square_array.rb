a37fb6825a7baee20f97a04d271d4f65c6bf491e
Sid He <xhe518@gmail.com>
2018-06-19 04:35:34 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,7 @@
 def square_array(array)
   array.each do |number|
-    new_number == number**2
-  new_array == []
+    new_number = number**2
+  new_array = []
   new_array.unshift(new_numbers)
   end
   new_array
